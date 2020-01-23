aec015bc38c01e802cf4e00a992da84150bb86a5
Sodiqlef <kehindeogundimu731@gmail.com>
2020-01-23 20:52:08 +0100

    another commit

---
+++ ukhome.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet"
+type="text/css"
+href="ukdesign.css">
+<link rel="stylesheet"> 
+<title> UK BOOMER OFFICIAL</title>
+</head>
+<body>
+<img src="logo2.jpg" alt="logo" class="logo"> <br> <br>
+<li class="inline">
+   <a href="ukhome.php">HOME</a>|
+    <a href="forecast">TODAY FORECAST</a>|
+    <a href="index.html">LOGOUT</a>|
+</li>
+
+<?php
+
+  echo "<h1>" . $_POST  ["username"] . "UK BOOMER OFFICIAL  </h1>"
+
+?>
+  <div class="background">
+    <div class="transbox">
+      <marquee> Welcome to UK boomer official website </marquee>
+    </div>
+  </dIV>
+  <div class="intro">
+      <p> MAKE MONEY WITH UK49'S BOOMING (8-10 TIMES IN A WEEK) ONLY BY JOINING OUR FORUM</p>      
+  </div>
+  <p> People are becoming 
+    <strong>'millionaires'</strong> on 
+    <strong> Uk Boomer Time</strong> 
+    through our daily forecast of Uk49's, Goslotto, Powerball, Megamillion and SA lotto.</p>
+  <p> with the help of the platform, all members will be getting 100% winning numbers on  all games listed above.
+  <h2> FREQUENTLY ASKED QUESTIONS 
+      (<strong class="span"> please read to the end</strong>)
+    </h2>
+    <h3> HOW DO I BOOM ON UK BOOMER? </h3>
+    <p> you only need to get our coupon code and register with us and you check our daily forecast
+    <h3> WILL I BOOM EVERYDAY? </h3>
+    <p> We can't assure you everyday booming, 
+      because there might be some problem from the technical crew but you boom 8-10 times weekly. 
+      Only the scammers will assure you 100% booming.</p>
+    <h3> HOW'S THE SUBSCRIPTION PACKAGE? </h3>
+    <table>
+      <tr>
+        <th> our packages </th>
+        <th> prices </th>
+      </tr>
+      <tr>
+        <td> 2 Week's Subscription package </td>
+        <td> <e class="before">R500</e> R300 </td>
+      </tr>
+      <tr>
+        <td> 1 month coupon package </td>
+        <td> <e class="before" >R800</e> R400 </td>
+      </tr>
+      <tr>
+        <td> 1<sup>1</Sup>/<sub>2</sub> month coupon package </td>
+        <td> <e class="before">R1000</e> R600 </td>
+      </tr>
+      <tr>
+        <td> 2 months coupon package </td>
+        <td> <e class="before">R1500 </e> R700 </td>
+      </table> <br>
+    <h3> HOW DO I MAKE PAYMENT?</h3>
+    <p> you make your payment by purchasing coupon code from our authorized coupon agent via bank or paying in a store, preferably.
+    <h3> HOW TO REGISTER?<h3>
+    <strong>UK BOOMER </strong> registration is easy as ABC, see steps below. 
+    <pre>
+    <ol>
+      <li> Click on registration at the top navigation menu or click <a href="ukRegistration.html">here</a></li>
+      <li> Fill the form</li>
+      <li> Input your purchased coupon code and sign up </li>
+      <li> That's all. Your account has been activated. </li>
+    </ol>
+    </pre>
+    <h2> MEMBERSHIP BENEFIT:
+      </pre>
+    <pre>
+      <ul>
+        <li> You Boom!!!</li>
+        <li> Free voucher as bonus sometimes by admin </li>
+        </ul>
+    </pre>
+
+        
+      <strong class="error" > NOTE </strong>
+    <pre>
+    <ul>
+    <li> We don't share numbers on sunday</li>
+    <li> All members can check games daily basis from 9.00am</li>
+    </ul>
+    </pre>
+     <a href="https://wa.me/27680992932?text=Hello!,%20i%20want%20to%20be%20a%20member%20of%20UKBOOMER%20username%20is%20"> GET COUPON CODE HERE </a> <br>
+    <br>
+    <a href="ukregistration.html"> REGISTER HERE </a>
+</body>
+
+<div class="center">
+<img src="logo1.jpg" alt="logo" >
+<footer>
+   copyright: 2019 
+</footer>
+</html>
